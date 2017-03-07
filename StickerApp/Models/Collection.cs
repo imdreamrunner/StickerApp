79c92491ecce89ca3b8cc847ff5c7c4c57ac7289
Ivor, Xinzi Zhou <zhouxz@garena.com>
2017-03-07 15:23:28 +0800

    Add collecton class.

---
+++ StickerApp/Models/Collection.cs
@@ -0,0 +1,13 @@
+﻿namespace StickerApp.Models
+{
+    public partial class Collection
+    {
+        public Collection()
+        {
+        }
+
+        public long CollectionId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
