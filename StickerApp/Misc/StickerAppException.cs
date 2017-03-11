54b45a1ddd60df92a7f3a864b34af8d6ef71b3bf
Ivor, Xinzi Zhou <zhouxz@garena.com>
2017-03-11 10:58:15 +0800

    Add XML comment for StickerAppException attribute.

--- StickerApp/Misc/StickerAppException.cs
+++ StickerApp/Misc/StickerAppException.cs
@@ -6,6 +6,10 @@ namespace StickerApp.Misc
     {
         public readonly string Error;
         public readonly string Reason;
+
+        /// <summary>
+        /// Suggested HTTP Error Code if this exception is thrown outside an action.
+        /// </summary>
         public readonly int? SuggestedHttpErrorCode;
 
         /// <summary>
